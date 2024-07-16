5d770001fa7cfaa910eb37a27ecdc36fa687e007
Jiating1110 <gohjiating345@gmail.com>
2024-07-17 00:05:30 +0800

    2

--- Forms.py
+++ Forms.py
@@ -1,14 +1,27 @@
 from wtforms import Form,StringField,PasswordField,validators
 from wtforms.validators import DataRequired,ValidationError
+import re
 
 def pwd_dic_check(form,field):
     file=open("wordlist.txt","r")
     for password in file:
         if password.strip().lower() in field.data.lower():
             raise ValidationError('Password contains a common word and is not allowed.')
+def pwd_check(form,field):
+    pwd=field.data
+    pattern=("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{10,20}$")
+    # if len(pwd)<10:
+    #     raise ValidationError('Password must have at least 10 characters')
+    if re.search(r"(.)\1{2}", pwd):
+        raise ValidationError('Not more than 2 identical characters in a row')
+    if not re.match(pattern,pwd):
+        raise ValidationError('Passoword must be at least 10 characters long,contain'
+                              'at least one uppercase and one lowercase characters,'
+                              'at least one numerals number'
+                              'at least one special characters')
 class RegisterForm(Form):
     username=StringField('Username',[validators.DataRequired()])
-    password=PasswordField('Password',[validators.DataRequired(),pwd_dic_check])
+    password=PasswordField('Password',[validators.DataRequired(),pwd_dic_check,pwd_check])
     email=StringField('Email',[validators.Email(message='Invalid Email format'),validators.DataRequired(message='Email address is required.')])
 
 class LoginForm(Form):
@@ -28,4 +41,4 @@ class VerifyEmail(Form):
 
 class ChangePassword(Form):
     newpwd=PasswordField('New Password',[validators.DataRequired()])
-    confirmpwd=PasswordField('Confirm Password',[validators.DataRequired(),pwd_dic_check])
+    confirmpwd=PasswordField('Confirm Password',[validators.DataRequired(),pwd_dic_check,pwd_check])
